320482cb886adf6ae0faab9d5e8cce1d7f6ea520
moritzmolch <moritz.molch@kit.edu>
2026-07-09 08:57:28 +0200

    Update path to 2024 ttbar test sample

--- build_scripts/test_build_2024.sh
+++ build_scripts/test_build_2024.sh
@@ -46,7 +46,7 @@ main () {
     declare -A test_files_list
     test_files_list[data]="root://cmsdcache-kit-disk.gridka.de//store/data/Run2024C/Muon0/NANOAOD/MINIv6NANOv15-v1/2530000/677e3bb0-8199-4ffb-83af-165410a7b7a6.root"
     #test_files_list[nmssm_Ybb]=""
-    test_files_list[ttbar]="root://cmsdcache-kit-disk.gridka.de//store/mc/RunIII2024Summer24NanoAODv15/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v3/2810000/f60b4a6c-2801-43b0-b542-6d933a71a396.root"
+    test_files_list[ttbar]="root://xrootd-cms.infn.it//store/mc/RunIII2024Summer24NanoAODv15/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v3/2810000/f60b4a6c-2801-43b0-b542-6d933a71a396.root"
     test_files_list[dyjets_amcatnlo_ll]="root://cmsdcache-kit-disk.gridka.de//store/mc/RunIII2024Summer24NanoAODv15/DYto2E-2Jets_Bin-MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v4/2540000/4dd4d960-192a-47b9-9bb4-7a6513c1a1de.root"
 
     # configure and compile the project
