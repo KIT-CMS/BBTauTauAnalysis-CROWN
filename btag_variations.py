09c294e7ea683f7da350153e0f4967175aebed77
moritzmolch <moritz.molch@kit.edu>
2026-08-11 12:23:09 +0200

    Add b jet tagging variations for shape-based and WP-based corrections

--- btag_variations.py
+++ btag_variations.py
@@ -1,244 +1,140 @@
 from code_generation.configuration import Configuration
-from code_generation.producer import Producer
 from code_generation.systematics import SystematicShift
+
 from .producers import scalefactors as scalefactors
+from .constants import SCOPES
 
 
-def add_btagVariations(
+def add_bjet_tagging_fixed_wp_shifts(
     configuration: Configuration,
-    bjet_id_sf_producer: Producer,
+    era: str,
 ):
-    #########################
-    # btagging shape uncertainties
-    #########################
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFUp",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_hf"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFDown",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_hf"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    """
+    Add systematic shifts for the working point (WP)-based b jet tagging scale
+    factors.
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_hfstats1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_hfstats1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    The procedure follows the [BTV recommendations](https://btv-wiki.docs.cern.ch/PerformanceCalibration/SFUncertaintiesAndCorrelations/#ak4-working-point-based-sfs-fixedwp-sfs).
+    Currently, a reduced uncertainty scheme (one era-correlated and one
+    era-uncorrelated uncertainty for b/c and for light-flavor jets,
+    respectively).
+    """
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_hfstats2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncHFstats2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_hfstats2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    # Producers that the variations are applied to
+    producers = [
+        scalefactors.BJetWPUParT_SF,
+    ]
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFUp",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_lf"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFDown",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_lf"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    # Scopes that the variations are applied to
+    scopes = SCOPES
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_lfstats1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_lfstats1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    # Samples to exclude (where b jet taggin already takes place on data jets)
+    exclude_samples = ["data", "embedding", "embedding_mc"]
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_lfstats2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncLFstats2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_lfstats2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+    # Add variations for b/c and for light-flavor jets independently
+    for jet_flavor in ["bc", "lf"]:
+        for direction in ["up", "down"]:
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr1Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_cferr1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr1Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_cferr1"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+            # Common uncertainty group correlated across eras
+            name = (
+                "btag"
+                + jet_flavor.upper()
+                + "Correlated"
+                + direction.capitalize()
+            )
+            shift_value = f"{direction}_correlated"
+            configuration.add_shift(
+                SystematicShift(
+                    name=name,
+                    shift_config={
+                        f"bjet_sf_variation_{jet_flavor}": shift_value,
+                    },
+                    producers={tuple(scopes): producers},
+                ),
+                exclude_samples=exclude_samples,
+            )
 
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr2Up",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "up_cferr2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="btagUncCFerr2Down",
-            shift_config={
-                ("mt", "et", "tt"): {"bjet_sf_variation": "down_cferr2"},
-            },
-            producers={
-                ("mt", "et", "tt"): {
-                    bjet_id_sf_producer,
-                }
-            },
-        ),
-        exclude_samples=["data", "embedding", "embedding_mc"],
-    )
+            # Common uncertainty group that is uncorrelated across era
+            name = (
+                "btag"
+                + jet_flavor.upper()
+                + era
+                + direction.capitalize()
+            )
+            shift_value = f"{direction}_uncorrelated"
+            configuration.add_shift(
+                SystematicShift(
+                    name=name,
+                    shift_config={
+                        f"bjet_sf_variation_{jet_flavor}": shift_value,
+                    },
+                    producers={tuple(scopes): producers},
+                ),
+                exclude_samples=exclude_samples,
+            )
+
+
+def add_bjet_tagging_shape_shifts(
+    configuration: Configuration,
+    era: str,
+):
+    """
+    Add systematic shifts for shape-based b jet tagging scale factors.
+
+    The procedure follows the [BTV recommendations](https://btv-wiki.docs.cern.ch/PerformanceCalibration/SFUncertaintiesAndCorrelations/#ak4-shape-correction-sfs-iterativefit).
+
+    Notes
+    -----
+
+    This function only adds the core shifts of the method (`"hf"`, `"lf"`,
+    ...). The shape-based scale factors also come along with dedicated `jes_*`
+    shifts that need to be applied together with the corresponding jet energy
+    scale shifts.
+    """
+
+    # Producers that the variations are applied to
+    producers = [
+        scalefactors.BJetShapePNet_SF,
+    ]
+
+    # Scopes that the variations are applied to
+    scopes = SCOPES
+
+    # Samples to exclude (where b jet taggin already takes place on data jets)
+    exclude_samples = ["data", "embedding", "embedding_mc"]
+
+    # Sources of b jet tagging uncertainties for different jet flavors
+    unc_groups = [
+        "hf",
+        "lf",
+        "hfstats1",
+        "hfstats2",
+        "lfstats1",
+        "lfstats2",
+        "cferr1",
+        "cferr2",
+        "hfstats1",
+        "hfstats2",
+        "lfstats1",
+        "lfstats2",
+    ]
+
+    # Add up and down variations for each uncertainty group
+    for direction in ["up", "down"]:
+        for source in unc_groups:
+            name = f"btag{source.capitalize()}{direction.capitalize()}"
+            shift_value = f"{direction}_{source}"
+            configuration.add_shift(
+                SystematicShift(
+                    name=name,
+                    shift_config={
+                        tuple(scopes): {"bjet_sf_variation": shift_value},
+                    },
+                    producers={
+                        tuple(scopes): producers,
+                    },
+                ),
+                exclude_samples=exclude_samples,
+            )
+
+    # Search for JES uncertainties
+    for 
