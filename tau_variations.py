820dc2ba7934edff3b85a7ec106095670750faea
moritzmolch <moritz.molch@kit.edu>
2026-08-12 15:34:51 +0200

    Refactor tau ID vs jet SF shifts

--- tau_variations.py
+++ tau_variations.py
@@ -1,11 +1,124 @@
 from code_generation.configuration import Configuration
-from code_generation.producer import Producer
+from code_generation.producer import Producer, ProducerGroup
 from code_generation.systematics import SystematicShift
 from .producers import scalefactors as scalefactors
 from .producers import pairselection as pairselection
 from .producers import muons as muons
 from .producers import electrons as electrons
 from .producers import taus as taus
+
+from .constants import ERAS_RUN2
+
+
+def _get_run2_2022_2023_tau_id_vs_jet_shifts(
+    era: str,
+    tau_id_vs_jet_algorithm: str,
+):
+    """
+    Shift specifications of the tau ID vs jet scale factor variations for 2022
+    and 2023.
+    """
+    return [
+        *[
+            {
+                "variation_name": f"stat{i}_dm{dm}",
+                "cms_name": f"CMS_eff_t_{tau_id_vs_jet_algorithm}_VSjet_dm_stat{i}_DM{dm}_{era}",
+            }
+            for i in range(1, 3)
+            for dm in [0, 1, 10, 11]
+        ],
+        {
+            "variation_name": "syst_alleras",
+            "cms_name": f"CMS_eff_t_{tau_id_vs_jet_algorithm}_VSjet_dm_syst_alleras",
+        },
+        {
+            "variation_name": f"syst_{era}",
+            "cms_name": f"CMS_eff_t_{tau_id_vs_jet_algorithm}_VSjet_dm_syst_{era}",
+        },
+    ]
+
+
+def _get_2024_2025_tau_id_vs_jet_shifts(
+    era: str,
+    tau_id_vs_jet_algorithm: str,
+):
+    """
+    Shift specifications of the tau ID vs jet scale factor variations for 2024
+    and 2025.
+    """
+
+    # Set DMs and pt bin edges to the ones used in the scale factor measurement
+    dm_bins = [0, 1, 10, 11]
+    pt_edges = [20, 40, 60, 200]
+
+    return [
+        *[
+            {
+                "variation_name": f"custom_dm{dm}_pt{pt_start}to{pt_stop}",
+                "cms_name": f"CMS_eff_t_{tau_id_vs_jet_algorithm}_VSjet_dm_DM{dm}_pt{pt_start}to{pt_stop}_{era}",
+            }
+            for dm in dm_bins
+            for pt_start, pt_stop in zip(pt_edges[:-1], pt_edges[1:])
+        ],
+    ]
+
+
+def add_tau_id_vs_jet_shifts(
+    configuration: Configuration,
+    era: str,
+    producers: list[Producer | ProducerGroup],
+    scopes: list[str],
+    tau_id_vs_jet_algorithm: str = "DeepTau2018v2p5",
+):
+    """
+    Add shifts for tau ID vs jet scale factors for the given era.
+     
+    The shifts follow the uncertainty scheme
+    [recommended by the TAU POG](https://tau-wiki.docs.cern.ch/Corrections/#id-scale-factors).
+
+    Two uncertainty schemes are implemented, depending on the data-taking era:
+
+    - For Run 2 and 2022/2023, the uncertainties are directly taken from the
+      correction file. The uncertainties are organized into statistical
+      uncertainties of the measurement and systematic uncertainties
+      corresponding to different correlation schemes (uncorrelated, correlated
+      across eras, correlated across DM bins and eras, ...).
+
+    - For 2024/2025, the uncertainties are decorrelated for the DM and pt bins,
+      in which the measurement have been performed.
+    """
+
+    # Get list of shifts. The shifts are organized as dictionaries with the key
+    # 'variation_name' for the variation to be used for the SF evaluation and
+    # 'cms_name' for the name of the shift to be used in the output.
+    shifts = None
+    if era in ERAS_RUN2 + [
+        "2022preEE",
+        "2022postEE",
+        "2023preBPix",
+        "2023postBPix",
+    ]:
+        shifts = _get_run2_2022_2023_tau_id_vs_jet_shifts(era, tau_id_vs_jet_algorithm)
+    elif era in ["2024", "2025"]:
+        shifts = _get_2024_2025_tau_id_vs_jet_shifts(era, tau_id_vs_jet_algorithm)
+    else:
+        raise NotImplementedError(f"Shifts for era {era} are not implemented.")
+
+    # Add up and down variation for each shift
+    for shift in shifts:
+        for direction in ["up", "down"]:
+            shift_value = f"{direction}_{shift['variation_name']}"
+            configuration.add_shift(
+                SystematicShift(
+                    name=f"{shift['cms_name']}{direction.capitalize()}",
+                    shift_config={
+                        tuple(scopes): {
+                            "tau_id_sf_vsjet_variation": shift_value,
+                        },
+                    },
+                    producers={tuple(scopes): producers},
+                ),
+            )
 
 
 def add_tauVariations(
@@ -21,177 +134,6 @@ def add_tauVariations(
 ):
     if sample == "embedding" or sample == "embedding_mc" or sample == "data":
         return configuration
-    #########################
-    # TauvsJetID scale factor shifts
-    #########################
-    # vsJet shifts et/mt, tau pt dependent
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau30to35Down",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau30to35": "down"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau30to35Up",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau30to35": "up"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau35to40Down",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau35to40": "down"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau35to40Up",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau35to40": "up"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau40to500Down",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau40to500": "down"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau40to500Up",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau40to500": "up"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau500to1000Down",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau500to1000": "down"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau500to1000Up",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau500to1000": "up"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau1000toInfDown",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau1000toinf": "down"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTau1000toInfUp",
-            shift_config={("et", "mt"): {"tau_sf_vsjet_tau1000toinf": "up"}},
-            producers={("et", "mt"): tau_id_vs_jet_sf_2_producer},
-        )
-    )
-    # vsJet shifts tt, tau dm dependent
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM0Down",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM0": "down"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM0Up",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM0": "up"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM1Down",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM1": "down"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM1Up",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM1": "up"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM10Down",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM10": "down"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM10Up",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM10": "up"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM11Down",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM11": "down"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
-    configuration.add_shift(
-        SystematicShift(
-            name="vsJetTauDM11Up",
-            shift_config={"tt": {"tau_sf_vsjet_tauDM11": "up"}},
-            producers={
-                "tt": [
-                    tau_id_vs_jet_sf_1_producer,
-                    tau_id_vs_jet_sf_2_producer,
-                ]
-            },
-        )
-    )
     #########################
     # TauvsEleID scale factor shifts
     #########################
@@ -533,5 +475,3 @@ def add_tauVariations(
             },
         )
     )
-
-    return configuration
