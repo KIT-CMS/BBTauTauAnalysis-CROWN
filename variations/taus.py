4ffc6c840929e6d63bb18e6afd9888e7a6f75afd
moritzmolch <moritz.molch@kit.edu>
2026-08-14 12:34:43 +0200

    Simplify setup of systematic shifts for taus

--- variations/taus.py
+++ variations/taus.py
@@ -1,6 +1,5 @@
 from code_generation.configuration import Configuration
 from code_generation.producer import Producer, ProducerGroup
-from code_generation.systematics import SystematicShift
 from .producers import scalefactors as scalefactors
 from .producers import pairselection as pairselection
 from .producers import muons as muons
@@ -9,42 +8,44 @@ from .producers import taus as taus
 
 from .constants import ERAS_RUN2
 
+from ._util import add_systematic_shift, KeyValueShift
+
 
 def _get_run2_2022_2023_tau_id_vs_jet_shifts(
     era: str,
     tau_id_algorithm: str,
-):
+) -> list[KeyValueShift]:
     """
     Shift specifications of the tau ID vs jet scale factor variations for 2022
     and 2023.
     """
     return [
         *[
-            {
-                "cms_name": f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_stat{i}_DM{dm}_{era}",
-                "shift_key": "tau_id_sf_vsjet_variation",
-                "shift_value": f"{{direction}}_stat{i}_dm{dm}",
-            }
+            KeyValueShift(
+                name=f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_stat{i}_DM{dm}_{era}",
+                key="tau_id_sf_vsjet_variation",
+                value=f"{{direction}}_stat{i}_dm{dm}",
+            )
             for i in range(1, 3)
             for dm in [0, 1, 10, 11]
         ],
-        {
-            "cms_name": f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_syst_alleras",
-            "shift_key": "tau_id_sf_vsjet_variation",
-            "shift_value": "{{direction}}_syst_alleras",
-        },
-        {
-            "cms_name": f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_syst_{era}",
-            "shift_key": "tau_id_sf_vsjet_variation",
-            "shift_value": f"{{direction}}_syst_{era}",
-        },
+        KeyValueShift(
+            name=f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_syst_alleras",
+            key="tau_id_sf_vsjet_variation",
+            value="{direction}_syst_alleras",
+        ),
+        KeyValueShift(
+            name=f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_syst_{era}",
+            key="tau_id_sf_vsjet_variation",
+            value=f"{{direction}}_syst_{era}",
+        ),
     ]
 
 
 def _get_2024_2025_tau_id_vs_jet_shifts(
     era: str,
     tau_id_algorithm: str,
-):
+) -> list[KeyValueShift]:
     """
     Shift specifications of the tau ID vs jet scale factor variations for 2024
     and 2025.
@@ -55,15 +56,13 @@ def _get_2024_2025_tau_id_vs_jet_shifts(
     pt_edges = [20, 40, 60, 200]
 
     return [
-        *[
-            {
-                "cms_name": f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_DM{dm}_pt{pt_start}to{pt_stop}_{era}",
-                "shift_key": "tau_id_sf_vsjet_variation",
-                "shift_value": f"{{direction}}_custom_dm{dm}_pt{pt_start}to{pt_stop}",
-            }
-            for dm in dm_bins
-            for pt_start, pt_stop in zip(pt_edges[:-1], pt_edges[1:])
-        ],
+        KeyValueShift(
+            name=f"CMS_eff_t_{tau_id_algorithm}_VSjet_dm_DM{dm}_pt{pt_start}to{pt_stop}_{era}",
+            key="tau_id_sf_vsjet_variation",
+            value=f"{{direction}}_custom_dm{dm}_pt{pt_start}to{pt_stop}",
+        )
+        for dm in dm_bins
+        for pt_start, pt_stop in zip(pt_edges[:-1], pt_edges[1:])
     ]
 
 
@@ -102,26 +101,15 @@ def add_tau_id_vs_jet_shifts(
         "2023preBPix",
         "2023postBPix",
     ]:
-        shifts = _get_run2_2022_2023_tau_id_vs_jet_shifts(era, tau_id_algorithm=tau_id_algorithm)
+        shifts = _get_run2_2022_2023_tau_id_vs_jet_shifts(era, tau_id_algorithm)
     elif era in ["2024", "2025"]:
-        shifts = _get_2024_2025_tau_id_vs_jet_shifts(era, tau_id_algorithm=tau_id_algorithm)
+        shifts = _get_2024_2025_tau_id_vs_jet_shifts(era, tau_id_algorithm)
     else:
         raise NotImplementedError(f"Shifts for era {era} are not implemented.")
 
     # Add up and down variation for each shift
     for shift in shifts:
-        for direction in ["up", "down"]:
-            configuration.add_shift(
-                SystematicShift(
-                    name=f"{shift['cms_name']}{direction.capitalize()}",
-                    shift_config={
-                        tuple(scopes): {
-                            shift["shift_key"]: shift["shift_value"].format(direction=direction),
-                        },
-                    },
-                    producers={tuple(scopes): producers},
-                )
-            )
+        add_systematic_shift(configuration, shift, producers, scopes=scopes)
 
 
 def add_tau_id_vs_e_shifts(
@@ -145,31 +133,18 @@ def add_tau_id_vs_e_shifts(
     # Independent shifts of the tau ID vs electron scale factor. Shifts are
     # decorrelated between 2D bins in DM x (barrel, endcap).
     shifts = [
-        {
-            "cms_name": f"CMS_fake_t_{tau_id_algorithm}_VSe_DM{dm}_{eta_region}_{era}",
-            "shift_key": "tau_id_sf_vsele_variation",
-            "shift_value": f"{{direction}}_custom_dm{dm}_{eta_region}",
-        }
+        KeyValueShift(
+            name=f"CMS_fake_t_{tau_id_algorithm}_VSe_DM{dm}_{eta_region}_{era}",
+            key="tau_id_sf_vsele_variation",
+            value=f"{{direction}}_custom_dm{dm}_{eta_region}",
+        )
         for dm in [0, 1, 10, 11]
         for eta_region in ["barrel", "endcap"]
     ]
 
     # Add up and down variation for each shift
     for shift in shifts:
-        for direction in ["up", "down"]:
-            configuration.add_shift(
-                SystematicShift(
-                    name=f"{shift['cms_name']}{direction.capitalize()}",
-                    shift_config={
-                        tuple(scopes): {
-                            shift["shift_key"]: shift["shift_value"].format(
-                                direction=direction
-                            ),
-                        },
-                    },
-                    producers={tuple(scopes): producers},
-                )
-            )
+        add_systematic_shift(configuration, shift, producers, scopes=scopes)
 
 
 def add_tau_id_vs_mu_shifts(
@@ -193,30 +168,17 @@ def add_tau_id_vs_mu_shifts(
     # Independent shifts of the tau ID vs muon scale factor. Shifts are
     # decorrelated between the different muon wheel regions. 
     shifts = [
-        {
-            "cms_name": f"CMS_fake_t_{tau_id_algorithm}_VSmu_{eta_region}_{era}",
-            "shift_key": "tau_id_sf_vsele_variation",
-            "shift_value": f"{{direction}}_custom_{eta_region}",
-        }
+        KeyValueShift(
+            name=f"CMS_fake_t_{tau_id_algorithm}_VSmu_{eta_region}_{era}",
+            key="tau_id_sf_vsele_variation",
+            value=f"{{direction}}_custom_{eta_region}",
+        )
         for eta_region in (f"wheel{i}" for i in range(1, 6))
     ]
 
     # Add up and down variation for each shift
     for shift in shifts:
-        for direction in ["up", "down"]:
-            configuration.add_shift(
-                SystematicShift(
-                    name=f"{shift['cms_name']}{direction.capitalize()}",
-                    shift_config={
-                        tuple(scopes): {
-                            shift["shift_key"]: shift["shift_value"].format(
-                                direction=direction
-                            ),
-                        },
-                    },
-                    producers={tuple(scopes): producers},
-                )
-            )
+        add_systematic_shift(configuration, shift, producers, scopes=scopes)
 
 
 def add_tau_es_shifts(
@@ -238,63 +200,52 @@ def add_tau_es_shifts(
     adopted.
     """
 
-    # Get scopes from the producer object
-    scopes = tuple(producer.scopes)
-
     # Specify properties of the shifts
     shifts = [
         # Shifts for genuine taus decaying hadronically
         *[
-            {
-                "cms_name": f"CMS_scale_t_{tau_id_algorithm}_DM{dm}_genTau_{era}",
-                "shift_key": "tau_es_variation",
-                "shift_value": f"{{direction}}_custom_genTau_dm{dm}",
-            }
+            KeyValueShift(
+                name=f"CMS_scale_t_{tau_id_algorithm}_DM{dm}_genTau_{era}",
+                key="tau_es_variation",
+                value=f"{{direction}}_custom_genTau_dm{dm}",
+            )
             for dm in [0, 1, 10, 11]
         ],
 
         # Shifts for electrons faking hadronic taus
         *[
-            {
-                "cms_name": f"CMS_scale_t_{tau_id_algorithm}_DM{dm}_genElectron_{eta_region}_{era}",
-                "shift_key": "tau_es_variation",
-                "shift_value": f"{{direction}}_custom_genEle_dm{dm}_{eta_region}",
-            }
+            KeyValueShift(
+                name=f"CMS_scale_t_{tau_id_algorithm}_DM{dm}_genElectron_{eta_region}_{era}",
+                key="tau_es_variation",
+                value=f"{{direction}}_custom_genEle_dm{dm}_{eta_region}",
+            )
             for dm in [0, 1, 10, 11]
             for eta_region in ["barrel", "endcap"]
         ],
 
         # Shifts for muons faking hadronic taus
         *[
-            {
-                "cms_name": f"CMS_scale_t_{tau_id_algorithm}_genMuon_{eta_region}_{era}",
-                "shift_key": "tau_es_variation",
-                "shift_value": f"{{direction}}_custom_genMu_{eta_region}",
-            }
+            KeyValueShift(
+                name=f"CMS_scale_t_{tau_id_algorithm}_genMuon_{eta_region}_{era}",
+                key="tau_es_variation",
+                value=f"{{direction}}_custom_genMu_{eta_region}",
+            )
             for eta_region in (f"wheel{i}" for i in range(1, 6))
         ],
     ]
 
     # Add up and down variation for each shift 
     for shift in shifts:
-        shift_key = shift["shift_key"]
-        cms_name = shift["cms_name"]
-        for direction in ["up", "down"]:
-            shift_value = shift["shift_value"].format(direction=direction)
-            configuration.add_shift(
-                SystematicShift(
-                    name=f"{cms_name}{direction.capitalize()}",
-                    shift_config={
-                        tuple(scopes): {
-                            shift_key: shift_value,
-                        },
-                    },
-                    producers={tuple(scopes): [producer]},
-                    # TODO understand why these producers need to be ignored
-                    ignore_producers={
-                        "et": [pairselection.LVEl1, electrons.VetoElectrons],
-                        "mt": [pairselection.LVMu1, muons.VetoMuons],
-                        "tt": [],
-                    },
-                )
-            )
+        add_systematic_shift(
+            configuration,
+            shift,
+            producer,
+            shift_kwargs={
+                # TODO understand why these producers need to be ignored
+                "ignore_producers": {
+                    "et": [pairselection.LVEl1, electrons.VetoElectrons],
+                    "mt": [pairselection.LVMu1, muons.VetoMuons],
+                    "tt": [],
+                },
+            },
+        )
